868fb4f02b779aaae9633cb57fac86070cfdd5ae
Carl Lewis Castillo <carllewiscastillo@gmail.com>
2023-08-26 16:47:46 -0400

    fix: add new field to model ticket

--- src/modules/tickets/model.ts
+++ src/modules/tickets/model.ts
@@ -1,4 +1,5 @@
 import * as mongoose from 'mongoose';
+import { Model } from 'mongoose';
 
 type Message = {
   sender: string;
@@ -9,14 +10,13 @@ export interface ITicket extends mongoose.Document {
   initialContext: string;
   chat: Message[];
   status: 'open' | 'pending' | 'resolved';
+  number: number;
 }
 
 const TicketSchema = new mongoose.Schema(
   {
-    initialContextil: {
+    initialContext: {
       type: String,
-      required: true,
-      unique: true,
     },
     status: {
       type: String,
@@ -25,12 +25,29 @@ const TicketSchema = new mongoose.Schema(
     chat: {
       type: Array<Message>,
     },
+    number: { type: Number, unique: true, default: 0 },
   },
   {
     versionKey: false,
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
   },
 );
+
+TicketSchema.pre<ITicket>('save', async function (next) {
+  if (this.isNew) {
+    try {
+      const TicketModel = mongoose.model('Ticket') as Model<ITicket>;
+      const lastOrder = await TicketModel.findOne().sort({ number: -1 }).select('number');
+      const nextNumber = lastOrder ? lastOrder.number + 1 : 1;
+      this.number = nextNumber;
+      next();
+    } catch (error: any) {
+      next(error);
+    }
+  } else {
+    next();
+  }
+});
 
 const Ticket = mongoose.model<ITicket>('Ticket', TicketSchema);
 
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Express } from 'express';
 import auth from './modules/auth/routes';
 import forms from './modules/forms/routes';
 import users from './modules/users/routes';
+import tickets from './modules/tickets/routes';
 
 export const setupRoutes = (app: Express) => {
   const prefix = '/api';
@@ -10,4 +11,5 @@ export const setupRoutes = (app: Express) => {
   app.use(`${prefix}/auth`, auth);
   app.use(`${prefix}/users`, users);
   app.use(`${prefix}/forms`, forms);
+  app.use(`${prefix}/tickets`, tickets);
 };
